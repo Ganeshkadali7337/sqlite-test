b8ab114342424e407f3cebeb29686329dd5a35a4
Ganeshkadali7337 <ganeshkadali0@gmail.com>
2024-04-30 12:41:44 +0530

    m

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(express.json()); // for parsing application/json
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
